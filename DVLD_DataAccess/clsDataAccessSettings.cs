498e7ff95e4bddc230cc39891e0c0abcb9c9837b
ibujnan <iboujnane.dev@gmail.com>
2026-08-14 00:10:05 +0200

    Add GetAllPeople method to person data access

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string connectionString = "Server = .; Database = DrivingAndVehicleLicenseDepartmentDB; User Id = sa; Password = 123456;";
+    }
+}
---
+++ DVLD_DataAccess/clsPersonData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public class clsPersonData
+    {
+       public static DataTable GetAllPeople()
+        {
+            DataTable peopleTable = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+            string query = "" +
+                "SELECT PersonID," +
+                "       NationalNo," +
+                "       FirstName," +
+                "       SecondName," +
+                "       ThirdName," +
+                "       LastName," +
+                "       DateOfBirth," +
+                "       Gendor," +
+                "       GendorType =" +
+                "           CASE" +
+                "               WHEN Gendor = 0 THEN 'Male'" +
+                "               WHEN Gendor = 1 THEN 'Female'" +
+                "           END," +
+                "       Address," +
+                "       Phone," +
+                "       Email," +
+                "       NationalityCountryID," +
+                "       ImagePath" +
+                "FROM People P" +
+                "INNER JOIN Countries C" +
+                "   ON P.NationalityCountryID = C.CountryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    peopleTable.Rows.Add(reader);
+                }
+                reader.Close();
+            } 
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return peopleTable;
+        }
+
+    }
+}
