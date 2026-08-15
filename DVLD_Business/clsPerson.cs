82ac9742209f62ea5be65833857d27629d19ceca
ibujnan <iboujnane.dev@gmail.com>
2026-08-15 07:03:24 +0200

    Add new person to person business logic

--- DVLD_Business/clsPerson.cs
+++ DVLD_Business/clsPerson.cs
@@ -1,5 +1,6 @@
 ﻿using DVLD_DataAccess;
 using System.Data;
+using System;
 
 namespace DVLD_Business
 {
@@ -9,6 +10,11 @@ namespace DVLD_Business
         public static DataTable GetAllPeople()
         {
             return clsPersonData.GetAllPeople();
+        }
+
+        public static int AddNew(string nationalNo, string firstName, string secondName, string thirdName, string lastName, DateTime dateOfBirth, short gender, string address, string phone, string email, int nationalityId, string imagePath)
+        {
+            return clsPerson.AddNew(nationalNo, firstName, secondName, thirdName, lastName, dateOfBirth, gender, address, phone, email, nationalityId, imagePath);
         }
     }
 }
