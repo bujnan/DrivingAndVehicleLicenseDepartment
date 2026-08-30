2b4280b1cb3485572c58f3cb7aab80eb07eddceb
ibujnan <iboujnane.dev@gmail.com>
2026-08-31 01:45:15 +0200

    Add login info UI to Add New User form

--- DrivingAndVehicleLicenseDepartment/User/frmAddNewUser.cs
+++ DrivingAndVehicleLicenseDepartment/User/frmAddNewUser.cs
@@ -16,5 +16,10 @@ namespace DrivingAndVehicleLicenseDepartment.User
         {
             InitializeComponent();
         }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tpLoginInfo;
+        }
     }
 }
